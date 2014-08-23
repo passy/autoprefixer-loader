bc98281291c1ace0707c79b20f593b40866ebb69
Andrey Sitnik <andrey@sitnik.ru>
2014-08-23 23:46:23 +0400

    Use CLI test runner

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,9 @@
 var gulp = require('gulp');
+var fs   = require('fs-extra');
+
+gulp.task('clean', function (done) {
+    fs.remove(__dirname + '/build', done);
+});
 
 gulp.task('lint', function () {
     var jshint = require('gulp-jshint');
@@ -9,4 +14,28 @@ gulp.task('lint', function () {
         .pipe(jshint.reporter('fail'));
 });
 
-gulp.task('default', ['lint']);
+gulp.task('build', ['clean'], function () {
+    var webpack = require('gulp-webpack');
+
+    return gulp.src('')
+        .pipe(webpack({
+            target: 'node',
+            context: __dirname + '/test/',
+            entry: './test.js',
+            output: {
+                path: __dirname + '/build/',
+                filename: "test.js",
+            }
+        }))
+        .pipe(gulp.dest('build/'));
+});
+
+gulp.task('test', ['build'], function () {
+    var mocha = require('gulp-mocha');
+    require('should');
+
+    return gulp.src('build/*.js', { read: false })
+        .pipe(mocha());
+});
+
+gulp.task('default', ['lint', 'test']);
