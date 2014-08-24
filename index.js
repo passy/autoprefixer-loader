7e7203340b9993abfbfaae85494ab41cf2d72345
Andrey Sitnik <andrey@sitnik.ru>
2014-08-24 23:09:27 +0400

    Set input file name and fix indent

--- index.js
+++ index.js
@@ -2,25 +2,26 @@ var loaderUtils = require('loader-utils');
 var autoprefixer = require('autoprefixer-core');
 
 module.exports = function (source) {
-  if (this.cacheable) {
-    this.cacheable();
-  }
+    if (this.cacheable) {
+        this.cacheable();
+    }
 
-  var params = loaderUtils.parseQuery(this.query);
+    var file = loaderUtils.getRemainingRequest(this);
+    var params = loaderUtils.parseQuery(this.query);
 
-  if (params.browsers && !Array.isArray(params.browsers)) {
-    params.browsers = params.browsers.split(',');
-  }
-  if (params.cascade == 'false') {
-    params.cascade = false;
-  }
+    if (params.browsers && !Array.isArray(params.browsers)) {
+        params.browsers = params.browsers.split(',');
+    }
+    if (params.cascade == 'false') {
+        params.cascade = false;
+    }
 
-  var options = { };
-  if (params.safe) {
-    delete params.safe;
-    options.safe = true;
-  }
+    var options = { from: file };
+    if (params.safe) {
+        delete params.safe;
+        options.safe = true;
+    }
 
-  var processed = autoprefixer(params).process(source, options);
-  this.callback(null, processed.css, processed.map);
+    var processed = autoprefixer(params).process(source, options);
+    this.callback(null, processed.css, processed.map);
 };
