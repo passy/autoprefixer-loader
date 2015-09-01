72dd4e3df0050a4cc8a9d68a61faa75abf532dec
Pascal Hartig <phartig@twitter.com>
2015-09-01 10:54:49 +0200

    Remove jshint

--- gulpfile.js
+++ gulpfile.js
@@ -7,15 +7,6 @@ gulp.task('clean', function (done) {
     fs.remove(path.join(__dirname, '/build'), done);
 });
 
-gulp.task('lint', function () {
-    var jshint = require('gulp-jshint');
-
-    return gulp.src(['index.js', 'test/*.js', 'gulpfile.js'])
-        .pipe(jshint())
-        .pipe(jshint.reporter('jshint-stylish'))
-        .pipe(jshint.reporter('fail'));
-});
-
 gulp.task('build', ['clean'], function () {
     var webpack = require('gulp-webpack');
 
@@ -40,4 +31,4 @@ gulp.task('test', ['build'], function () {
         .pipe(mocha());
 });
 
-gulp.task('default', ['lint', 'test']);
+gulp.task('default', ['test']);
